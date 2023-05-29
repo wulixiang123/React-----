0ae136fba20a8a706f848683a480ea4ede765424
liuyuanbo <1131107655@qq.com>
2023-05-29 14:52:54 +0800

    React简易轮播图实现

--- day02-React_learn/src/App.jsx
+++ day02-React_learn/src/App.jsx
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
-import imgSrc from './assets/images/1.jpg'
+import './App.css'
 export default class App extends Component {
     state = {
         index:1
     }
+    prev(index){
+        index--;
+        if(index<1){
+            index = 4
+        }
+        this.setState({
+            index
+        })
+    }
+    next(index){
+        index++;
+        if(index>4){
+            index = 1
+        }
+        this.setState({
+            index
+        })
+    }
   render() {
     let {index} = this.state
     return (
-      <div>
-        <h3>网络图片</h3>
-        <img src="https://images.unsplash.com/photo-1520808663317-647b476a81b9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218MHx8YW5pbWFsfHx8fHx8MTY4NTMzOTk2Ng&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=500" alt="" />
-        <h3>本地图片-require</h3>
-        <img src={require(`./assets/images/${index}.jpg`)} alt="" />
-        <h3>本地图片-import</h3>
-        <img src={imgSrc} alt="" />
+      <div className='box'>
+        <img src={require(`./assets/images/${index}.webp`)} alt="" />
+        <span onClick={()=>this.prev(index)}>&lt;</span>
+        <span id='right' onClick={()=>this.next(index)}>&gt;</span>
       </div>
     )
   }
