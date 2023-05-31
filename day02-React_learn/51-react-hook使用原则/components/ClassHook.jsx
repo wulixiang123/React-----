e7565363c41f7b0c253876b2032d953078c9325e
liuyuanbo <1131107655@qq.com>
2023-05-31 21:02:11 +0800

    react的hook使用原则

---
+++ day02-React_learn/51-react-hook使用原则/components/ClassHook.jsx
@@ -0,0 +1,12 @@
+import React, { Component, useState } from 'react'
+
+export default class ClassHook extends Component {
+
+    render() {
+        // let [count, setCount] = useState(1000);
+        return (
+            <div>ClassHook</div>
+
+        )
+    }
+}
