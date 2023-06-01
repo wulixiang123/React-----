56787863d8547e39914e03c42efb1a155718c8b2
liuyuanbo <1131107655@qq.com>
2023-06-01 13:43:22 +0800

    猫抓老鼠自定义hook版本

---
+++ day02-React_learn/54-猫抓老鼠-自定义hook版本/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Mouse from './components/Mouse'
+import Cat from './components/Cat'
+
+export default function App() {
+  return (
+    <div>
+      <Mouse/>
+      <Cat/>
+    </div>
+  )
+}
--- day02-React_learn/src/hook/userSelfHook.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-function useSelfHook(){//自定义的hook函数
-    let [count,setCount] = useState(100)
-    return count
-}
-export default useSelfHook
