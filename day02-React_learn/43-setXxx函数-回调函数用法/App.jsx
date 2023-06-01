4b7b466988322b0dd1ac94c5780233dedb3f3a2b
liuyuanbo <1131107655@qq.com>
2023-06-01 23:44:00 +0800

    扩展

--- day02-React_learn/43-setXxx函数-回调函数用法/App.jsx
+++ day02-React_learn/43-setXxx函数-回调函数用法/App.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from 'react'
 
 export default function App() {
-    let [msg,setMsg] = useState('atguigu')
+    let [msg,setMsg] = useState('atguigu')//msg的值为atguigu,setMsg修改msg的值
     useEffect(()=>{
         setTimeout(()=>{
-            setMsg((msg)=>{
+            /**
+             * setMsg 参数可以是一个回调函数，回调函数的参数能够
+             * 获取到 useState中缓存的最新的状态值，
+             * 回调函数的返回值，就是设置的最新的 msg状态值
+             */
+
+
+            // 方式一
+            //setMsg(msg + '-');// 说明此处的msg 只能取到初始值 atguigu
+            // 方式二
+            setMsg((msg)=>{//这里必须要使用箭头函数 不然拿不到最新的被修改过的值
                 return msg + '*'
             })
         },2000)
-    },[])
+    },[])//挂载阶段
   return (
     <div>
         <p>msg:{msg}</p>
