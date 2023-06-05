6f96bf2ad73891afbdaf2ae9c82a9d771ee02020
liuyuanbo <1131107655@qq.com>
2023-06-05 08:47:57 +0800

    NavLink和Link的区别

---
+++ day02-React_learn/77-NavLink-Link使用及区别/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Routes, Route, NavLink, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import User from './pages/User'
+import './App.css'
+export default function App() {
+    return (
+        <div>
+            <h3>App</h3>
+            {/* <ul>
+                <li><a href="/login">登录</a></li>
+                <li><a href="/user">用户中心</a></li>
+                <li><a href="/home">首页</a></li>
+            </ul> */}
+
+            {/* NavLink 激活的路由有高亮样式类名 active【默认】 */}
+            <ul>
+                <li><NavLink to='/login'>登录</NavLink></li>
+                <li><NavLink to='/user'>用户中心</NavLink></li>
+                <li><NavLink to='/home'>首页</NavLink></li>
+            </ul>
+            {/* Link可以生成无刷新的跳转链接，但是没有高亮样式类名 */}
+            <ul>
+                <li><Link to='/login'>登录</Link></li>
+                <li><Link to='/user'>用户中心</Link></li>
+                <li><Link to='/home'>首页</Link></li>
+            </ul>
+            {/* 
+                4. 配置路由 path 和路由组件的映射关系
+                   path 指定 路径
+                   element： 指定该路径对应展示的路由组件
+            */}
+            <Routes>
+                <Route path='/login' element={<Login />}></Route>
+                <Route path='/home' element={<Home />}></Route>
+                <Route path='/user' element={<User />}></Route>
+            </Routes>
+        </div>
+    )
+}
--- day02-React_learn/src/App.jsx
+++ day02-React_learn/src/App.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import Son from './components/Son'
-
+import { Routes, Route, NavLink, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import User from './pages/User'
+import './App.css'
 export default function App() {
   return (
     <div>
-      <Son/>
+      <h3>App</h3>
+      <ul>
+        <li><a href="/login">登录</a></li>
+        <li><a href="/user">用户中心</a></li>
+        <li><a href="/home">首页</a></li>
+      </ul>
+
+      <ul>
+        <li><NavLink to='/login'>登录</NavLink></li>
+        <li><NavLink to='/user'>用户中心</NavLink></li>
+        <li><NavLink to='/home'>首页</NavLink></li>
+      </ul>
+
+      <ul>
+        <li><Link to='/login'>登录</Link></li>
+        <li><Link to='/user'>用户中心</Link></li>
+        <li><Link to='/home'>登录</Link></li>
+      </ul>
+
+      <Routes>
+                <Route path='/login' element={<Login />}></Route>
+                <Route path='/home' element={<Home />}></Route>
+                <Route path='/user' element={<User />}></Route>
+            </Routes>
     </div>
   )
 }
