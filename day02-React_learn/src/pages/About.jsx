1daf32b418f0e541f4ee872b71e3159ff03b8e66
liuyuanbo <1131107655@qq.com>
2023-06-05 17:27:53 +0800

    添加商品-列表-购物车列表静态页面

--- day02-React_learn/src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function About() {
-  return (
-    <div>About 页面</div>
-  )
-}
---
+++ day02-React_learn/src/pages/CarList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function CarList() {
+  return (
+    <>
+        <h3>购物车列表</h3>
+        <ul>
+            <li>
+                <h3>商品名称:小米手机</h3>
+                <p>价格:1999</p>
+                <p>数量<button>-</button> 6 <button> + </button></p>
+            </li>
+            <li>
+                <h3>商品名称:小米手机</h3>
+                <p>价格:1999</p>
+                <p>数量<button>-</button> 6 <button> + </button></p>
+            </li>
+        </ul>
+    </>
+  )
+}
