bc9b0bbd6a6fca00bc018fc254790f4ae15f9d4a
liuyuanbo <1131107655@qq.com>
2023-06-05 10:49:00 +0800

    二级路由页面练习

---
+++ day02-React_learn/82-二级路由页面练习/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+//1. 安装 npm i react-router-dom  2. 导入
+import { BrowserRouter } from 'react-router-dom';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+     // 3. 包裹跟组件App
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+
+
