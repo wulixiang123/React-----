cebabd36d5c0a995efcad73bd48497a2fce16b54
liuyuanbo <1131107655@qq.com>
2023-06-06 20:30:26 +0800

    redux与react结合的问题

--- day02-React_learn/98-redux与react结合及问题呈现/store/index.js
+++ day02-React_learn/98-redux与react结合及问题呈现/store/index.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import count from './slice/countSlice'
-
-const store = configureStore({
+import {configureStore} from '@reduxjs/toolkit'//创建仓库存储数据
+import count from './slice/countSlice'//引入数据
+const store = configureStore({//创建仓库
     reducer:{
-        count
+        //reducer指定应用程序状态的变化方式。它是一个纯函数，
+        //接收当前状态和发生的动作作为输入，返回新状态作为输出。
+       count
     }
 })
-
-export default store;
+export default store
--- day02-React_learn/src/index.js
+++ day02-React_learn/src/index.js
@@ -1,16 +1,8 @@
-import store from "./store";
-import { addGoods } from "./store/slice/goodsSlice";
-import { addCar } from "./store/slice/carSlice";
-//监听store仓库的变化,变化则触发回调
-store.subscribe(()=>{
-    console.log(store.getState());
-})
-//添加商品
-store.dispatch(addGoods({gname:'小米手机',price:1999}))
-store.dispatch(addGoods({gname:'华为手机',price:2999}))
-console.log(store.getState().goods.goodsList);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-// 添加购物车
-store.dispatch(addCar(store.getState().goods.goodsList[0]))
-store.dispatch(addCar(store.getState().goods.goodsList[0]))
-store.dispatch(addCar(store.getState().goods.goodsList[1]))
+root.render((
+    <App/>
+))
