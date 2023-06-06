330bbfa294906305eae254833fa2d9cf97eea529
liuyuanbo <1131107655@qq.com>
2023-06-06 18:45:01 +0800

    创建仓库获取仓库并修改仓库数据

--- day02-React_learn/src/index.js
+++ day02-React_learn/src/index.js
@@ -1,11 +1,12 @@
-import {createSlice} from '@reduxjs/toolkit'//下载并导入
+//configureStore是创建仓库的必要条件
+import {createSlice,configureStore} from '@reduxjs/toolkit'//下载并导入
 const countSlice = createSlice({
     name:'count',//固定写法 count是每一个方法中的type属性
     initialState:{//数据
         num:1
     },
     reducers:{//定义方法的容器
-        addNum(state,{payload}){
+        addNum(state,{payload}){//payload就是实参传过来的值
             state.num += payload
         },
         decNum(state,{payload}){
@@ -18,5 +19,18 @@ console.log(countSlice);
 
 const {addNum,decNum} = countSlice.actions//解构方法存到actions中
 
-console.log('addNum(1):',addNum(3));
+console.log('addNum(3):',addNum(3));
 console.log('decNum(1)',decNum(1));
+
+const store = configureStore({//创建仓库
+    reducer:{
+        count:countSlice.reducer
+    }
+})
+// console.log(store);//store仓库中常用的方法有 1.getState() 2.dispatch
+// console.log(store.getState());
+// console.log(store.getState().count);
+console.log(store.getState().count.num);//获取仓库的数据
+
+store.dispatch(addNum(3))//store仓库中的dispatch方法接收一个方法并执行方法
+console.log(store.getState().count);
