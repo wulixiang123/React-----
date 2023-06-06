180ecb28bf93beaa793a7f96595b5574aed903ff
liuyuanbo <1131107655@qq.com>
2023-06-06 20:07:52 +0800

    store模块化

--- day02-React_learn/97-store模块化/index.js
+++ day02-React_learn/97-store模块化/index.js
@@ -1,22 +1,16 @@
-/**
- * 添加商品、添加购物车练习
- * 1. 两个切片：商品切片、购物车切片
- * 2. 商品切片：添加商品
- * 3. 购物车切片： 添加购物车、修改购买数量
- * 
- */
 import store from "./store";
 import { addGoods } from "./store/slice/goodsSlice";
 import { addCar } from "./store/slice/carSlice";
-
-store.subscribe(()=>{//6.监听仓库
+//监听store仓库的变化,变化则触发回调
+store.subscribe(()=>{
     console.log(store.getState());
 })
-//7.添加商品
-store.dispatch(addGoods({gname:'小米手机',price:3999}))
-store.dispatch(addGoods({gname:'华为手机',price:6999}))
+//添加商品
+store.dispatch(addGoods({gname:'小米手机',price:1999}))
+store.dispatch(addGoods({gname:'华为手机',price:2999}))
+console.log(store.getState().goods.goodsList);
 
-// 添加到购物车
+// 添加购物车
 store.dispatch(addCar(store.getState().goods.goodsList[0]))
 store.dispatch(addCar(store.getState().goods.goodsList[0]))
 store.dispatch(addCar(store.getState().goods.goodsList[1]))
--- day02-React_learn/src/index.js
+++ day02-React_learn/src/index.js
@@ -1,65 +1,6 @@
-//configureStore是创建仓库的必要条件
-import {createSlice,configureStore} from '@reduxjs/toolkit'//下载并导入
-
-/**
- * 添加商品、添加购物车练习
- * 1. 两个切片：商品切片、购物车切片
- * 2. 商品切片：添加商品
- * 3. 购物车切片： 添加购物车、修改购买数量
- * 
- */
-//商品切片
-const goodsSlice = createSlice({
-    name:'goods',//固定写法 goods是每一个方法中的type属性
-    initialState:{//数据
-        goodsList:[]
-    },
-    reducers:{//定义方法的容器
-        addGoods(state,{payload}){
-            state.goodsList = [...state.goodsList,{
-                id:Math.random().toString(36).slice(2),
-                ...payload
-            }]
-        }
-    }
-})
-
-const {addGoods} = goodsSlice.actions//解构方法存到actions中
-
-// 购物车切片
-const carSlice = createSlice({
-    name:'Car',//必须写name,Car是每个方法中的type属性
-    initialState:{//数据
-        carList:[]
-    },
-    reducers:{//定义方法的容器,所有方法在此定义
-        addCar(state,{payload}){
-            //carList中的id等于我们传的数据的id
-            let index = state.carList.findIndex(item=>item.id === payload.id)
-            if(index === -1){//判断数据id是否为假,就是说有没有这个id,如果没有原封不动展示
-                state.carList = [...state.carList,{
-                    ...payload,
-                    buyNam:1//购买数量
-                }]
-            }else{//否则+1
-                state.carList[index].buyNam += 1
-            }
-            
-        }
-    }
-})
-
-const {addCar} = carSlice.actions//解构方法存到actions中
-
-const store = configureStore({//创建仓库
-    reducer:{
-        //reducer指定应用程序状态的变化方式。它是一个纯函数，
-        //接收当前状态和发生的动作作为输入，返回新状态作为输出。
-        goods:goodsSlice.reducer,
-        car:carSlice.reducer
-    }
-})
-
+import store from "./store";
+import { addGoods } from "./store/slice/goodsSlice";
+import { addCar } from "./store/slice/carSlice";
 //监听store仓库的变化,变化则触发回调
 store.subscribe(()=>{
     console.log(store.getState());
