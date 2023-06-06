8601e567548facb5f1ca4d5ce6cb05a303978fc0
liuyuanbo <1131107655@qq.com>
2023-06-06 19:26:23 +0800

    添加商品

--- day02-React_learn/94-添加商品/index.js
+++ day02-React_learn/94-添加商品/index.js
@@ -1,3 +1,6 @@
+//configureStore是创建仓库的必要条件
+import {createSlice,configureStore} from '@reduxjs/toolkit'//下载并导入
+
 /**
  * 添加商品、添加购物车练习
  * 1. 两个切片：商品切片、购物车切片
@@ -5,15 +8,14 @@
  * 3. 购物车切片： 添加购物车、修改购买数量
  * 
  */
-import {createSlice, configureStore} from '@reduxjs/toolkit'//1.引入
 
-const goodsSlice = createSlice({//2.商品切片
-    name:'goods',
-    initialState:{
+const goodsSlice = createSlice({
+    name:'goods',//固定写法 count是每一个方法中的type属性
+    initialState:{//数据
         goodsList:[]
     },
-    reducers:{
-        addGoods(state,{payload}){//3.添加商品
+    reducers:{//定义方法的容器
+        addGoods(state,{payload}){
             state.goodsList = [...state.goodsList,{
                 id:Math.random().toString(36).slice(2),
                 ...payload
@@ -22,17 +24,20 @@ const goodsSlice = createSlice({//2.商品切片
     }
 })
 
-const {addGoods} = goodsSlice.actions;//4.获取商品
+const {addGoods} = goodsSlice.actions//解构方法存到actions中
 
-const store = configureStore({//5.创建仓库
+const store = configureStore({//创建仓库
     reducer:{
+        //reducer指定应用程序状态的变化方式。它是一个纯函数，
+        //接收当前状态和发生的动作作为输入，返回新状态作为输出。
         goods:goodsSlice.reducer
     }
 })
 
-store.subscribe(()=>{//6.监听仓库
+//监听store仓库的变化,变化则触发回调
+store.subscribe(()=>{
     console.log(store.getState());
 })
-//7.添加商品
-store.dispatch(addGoods({gname:'小米手机',price:3999}))
-store.dispatch(addGoods({gname:'华为手机',price:6999}))
+//添加商品
+store.dispatch(addGoods({gname:'小米手机',price:1999}))
+console.log(store.getState().goods.goodsList[0]);
--- day02-React_learn/src/index.js
+++ day02-React_learn/src/index.js
@@ -1,45 +1,43 @@
 //configureStore是创建仓库的必要条件
 import {createSlice,configureStore} from '@reduxjs/toolkit'//下载并导入
-const countSlice = createSlice({
-    name:'count',//固定写法 count是每一个方法中的type属性
+
+/**
+ * 添加商品、添加购物车练习
+ * 1. 两个切片：商品切片、购物车切片
+ * 2. 商品切片：添加商品
+ * 3. 购物车切片： 添加购物车、修改购买数量
+ * 
+ */
+
+const goodsSlice = createSlice({
+    name:'goods',//固定写法 count是每一个方法中的type属性
     initialState:{//数据
-        num:1
+        goodsList:[]
     },
     reducers:{//定义方法的容器
-        addNum(state,{payload}){//payload就是实参传过来的值
-            state.num += payload
-        },
-        decNum(state,{payload}){
-            state.num -= payload
+        addGoods(state,{payload}){
+            state.goodsList = [...state.goodsList,{
+                id:Math.random().toString(36).slice(2),
+                ...payload
+            }]
         }
     }
 })
 
-console.log(countSlice);
-
-const {addNum,decNum} = countSlice.actions//解构方法存到actions中
-
-console.log('addNum(3):',addNum(3));
-console.log('decNum(1)',decNum(1));
+const {addGoods} = goodsSlice.actions//解构方法存到actions中
 
 const store = configureStore({//创建仓库
     reducer:{
-        count:countSlice.reducer
+        //reducer指定应用程序状态的变化方式。它是一个纯函数，
+        //接收当前状态和发生的动作作为输入，返回新状态作为输出。
+        goods:goodsSlice.reducer
     }
 })
-// console.log(store);//store仓库中常用的方法有 1.getState() 2.dispatch
-// console.log(store.getState());
-// console.log(store.getState().count);
-console.log(store.getState().count.num);//获取仓库的数据
-
-store.dispatch(addNum(3))//store仓库中的dispatch方法接收一个方法并执行方法
-console.log(store.getState().count);
 
 //监听store仓库的变化,变化则触发回调
-const unsubscribe = store.subscribe(()=>{
-    console.log(store.getState(),'111');
+store.subscribe(()=>{
+    console.log(store.getState());
 })
-store.dispatch(addNum(3))
-store.dispatch(decNum(1))
-unsubscribe()//取消监听
-store.dispatch(addNum(100))
+//添加商品
+store.dispatch(addGoods({gname:'小米手机',price:1999}))
+console.log(store.getState().goods.goodsList[0]);
