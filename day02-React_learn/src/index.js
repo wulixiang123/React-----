841f1afdd1164dc1a48db2bfb204001000faa064
liuyuanbo <1131107655@qq.com>
2023-06-06 19:53:22 +0800

    添加购物车02

--- day02-React_learn/src/index.js
+++ day02-React_learn/src/index.js
@@ -34,10 +34,17 @@ const carSlice = createSlice({
     },
     reducers:{//定义方法的容器,所有方法在此定义
         addCar(state,{payload}){
-            state.carList = [...state.carList,{
-                ...payload,
-                buyNam:1
-            }]
+            //carList中的id等于我们传的数据的id
+            let index = state.carList.findIndex(item=>item.id === payload.id)
+            if(index === -1){//判断数据id是否为假,就是说有没有这个id,如果没有原封不动展示
+                state.carList = [...state.carList,{
+                    ...payload,
+                    buyNam:1//购买数量
+                }]
+            }else{//否则+1
+                state.carList[index].buyNam += 1
+            }
+            
         }
     }
 })
@@ -48,7 +55,8 @@ const store = configureStore({//创建仓库
     reducer:{
         //reducer指定应用程序状态的变化方式。它是一个纯函数，
         //接收当前状态和发生的动作作为输入，返回新状态作为输出。
-        goods:goodsSlice.reducer
+        goods:goodsSlice.reducer,
+        car:carSlice.reducer
     }
 })
 
@@ -62,5 +70,6 @@ store.dispatch(addGoods({gname:'华为手机',price:2999}))
 console.log(store.getState().goods.goodsList);
 
 // 添加购物车
+store.dispatch(addCar(store.getState().goods.goodsList[0]))
 store.dispatch(addCar(store.getState().goods.goodsList[0]))
 store.dispatch(addCar(store.getState().goods.goodsList[1]))
