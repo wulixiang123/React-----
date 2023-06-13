0d4601774ef395fffa50c0fea75a9aaa75f07e8e
liuyuanbo <1131107655@qq.com>
2023-06-13 11:18:27 +0800

    医院列表渲染

--- day05_react-project/syt-admin-react-base/src/api/hospital/hospitalList.ts
+++ day05_react-project/syt-admin-react-base/src/api/hospital/hospitalList.ts
@@ -1,5 +1,5 @@
 import {request} from '@utils/http'
-import { IDepartmentList, IDistrictList, IHospitalDetailResponse, IHospitalListParams, IHospitalListResponse } from './model/hospitalListTypes'
+import { IDepartmentList, IDistrictList, IDoctorList, IHospitalDetailResponse, IHospitalListParams, IHospitalListResponse, IScheduleResponse } from './model/hospitalListTypes'
 /**
  * 获取地区列表
  * @param id 
@@ -43,4 +43,14 @@ export const toggleState = (id:string,status:number)=>{
 // 根据医院编号获取科室列表
 export const getDepartmentList = (hoscode:string) => {
     return request.get<any,IDepartmentList>('/admin/hosp/department/' + hoscode)
+}
+
+// 科室排班日期分页数据
+export const getScheduleList = (page:number,limit:number,hoscode:string,depcode:string)=>{
+    return request.get<any, IScheduleResponse>(`/admin/hosp/schedule/getScheduleRule/${page}/${limit}/${hoscode}/${depcode}`)
+}
+
+// 获取排班医生列表
+export const getDoctorList = (hoscode:string,depcode:string,workDate:string)=>{
+    return request.get<any,IDoctorList>(`/admin/hosp/schedule/findScheduleList/${hoscode}/${depcode}/${workDate}`)
 }
